39d0543d32b953625a8dd6e28f97fd1eafed1368
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2024-06-30 11:10:15 +0300

    Update funny.js

--- funny.js
+++ funny.js
@@ -2,7 +2,7 @@
     'use strict';
 
     // Define the new favicon URL
-    var newFaviconURL = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fuser_1077114&psig=AOvVaw2f6dG40CR0GC7vwuXoKK7g&ust=1719821099992000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPjf6oDvgocDFQAAAAAdAAAAABAE';
+    var newFaviconURL = '';
 
     // Find existing favicon element
     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
