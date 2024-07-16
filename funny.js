585e28436f8992bb5310f91316f5b701c82976b9
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2024-07-16 15:54:34 +0300

    Update funny.js

--- funny.js
+++ funny.js
@@ -1,22 +1,4 @@
 (function() {
     'use strict';
 
-    // Define the new favicon URL
-    var newFaviconURL = '';
-
-    // Find existing favicon element
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = newFaviconURL;
-
-    // Remove any existing favicons
-    var head = document.getElementsByTagName('head')[0];
-    var existingIcons = head.querySelectorAll("link[rel*='icon']");
-    existingIcons.forEach(function(icon) {
-        head.removeChild(icon);
-    });
-
-    // Add the new favicon
-    head.appendChild(link);
 })();
