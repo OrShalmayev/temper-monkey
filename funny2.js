e9dec7839fc8cd862d478a869357683d625f730a
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2024-09-22 14:37:27 +0300

    wow

--- funny2.js
+++ funny2.js
@@ -1,4 +1,15 @@
 (function() {
     'use strict';
-    console.log('wow');
+
+    function replaceText(node) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            node.textContent = node.textContent.replace(/\bthis\b/gi, 'she');
+        } else {
+            node.childNodes.forEach(replaceText);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        replaceText(document.body);
+    });
 })();
